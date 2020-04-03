7f64532bbd24470ca9d5c5889b9149800edaa859
Louis James <ljame002@gold.ac.uk>
2020-04-03 14:46:43 +0100

    Readme update and charlie working

--- charlie.py
+++ charlie.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-import math,binascii,time,optparse
+import math,binascii,time,optparse,json
 
 def int_to_string(i):
     # convert int to hex, unhexlify and decode
     return binascii.unhexlify(hex(i)[2:]).decode('utf-8')
 
-def brute(m, d, n):
+def brute(d, n, c):
+    m=None
     while m is None:
         print(d)
         try:
@@ -15,8 +16,9 @@ def brute(m, d, n):
             pass
         d += 1
 
-        # time.sleep(.0001)
-    print(m)
+        time.sleep(.00001)
+    print("d: ", d)
+    print("m: ", m)
 
 def main():
     m = None
@@ -27,15 +29,18 @@ def main():
     (options, args) = parser.parse_args()
 
     if(options.args):
-        print(options.args)
+        # print(options.args)
         c=open(options.args[0], "r")
-        cipher=c.read()
+        cipher=json.load(c)
         k=open(options.args[1], "r")
-        public_key=k.read()
+        public_key=json.load(k)
         print(cipher)
+        print("PUBLIC KEY:")
         print(public_key)
+
+        brute(0, public_key['n'], cipher["cipher"])
     else:
-        print("")
+        print("Specify ciphertext and public key with -m")
 
     # brute()
 
